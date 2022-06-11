0eee5e6d5bcd5b2cdd62b1dd0730197fdc5b4eb7
kyznecov <58451637+kyznecov@users.noreply.github.com>
2022-06-11 17:52:46 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,74 @@
+"use strict";
+
+new Swiper(".cardSwiper", {
+  slidesPerView: 3,
+  //effect: "coverflow",
+  //coverflowEffect: {
+    //rotate: 50,
+    //modifier: 1,
+    //slideShadows: true,
+    //depth: 100,
+  //},
+  centeredSlides: true,
+  spaceBetween: 20,
+  // spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 3,
+});
+
+
+new Swiper(".teamSwiper", {
+  slidesPerView: 3,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    modifier: 1,
+    slideShadows: true,
+    depth: 100,
+  },
+  centeredSlides: true,
+  // spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 1,
+});
+
+new Swiper(".rewiev", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 20,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 1,
+});
